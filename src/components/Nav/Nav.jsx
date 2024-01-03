3c083f470171f8a923f5a20abd538d250773c7fa
Joshua Duenow <joshjduenow@gmail.com>
2024-01-03 11:57:40 -0600

    some styling. Pokeball home button and pokemon themed text.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Collect'Em All</h2>
+        <h2 className="nav-title">Collect'Em All !</h2>
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
@@ -24,8 +24,8 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
-              Home
+            <Link className="pokeball" to="/user">
+            
             </Link>
 
             <Link className="navLink" to="/info">
