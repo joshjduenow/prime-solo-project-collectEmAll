d7c28d34f7432ea04197913b422f5ff1c287e066
Joshua Duenow <joshjduenow@gmail.com>
2024-01-04 10:18:15 -0600

    add button styling

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,10 +1,12 @@
-
+import "./CardItem.css"
 
 export default function CardItem() {
     return (
         <div className="cardItem">
-        <button>Add</button>
-        <button>🖤</button>
+        <button class="button-74" role="button">Add</button>
+        <button class="button-74" role="button">Save</button>
+        <button class="button-74" role="button">Archive</button>
+        <button class="button-74" role="button">Delete</button>
         <p>Card Description</p>
         </div>
 
