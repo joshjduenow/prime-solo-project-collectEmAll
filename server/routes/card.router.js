ebcee8637516ea8bc7ed837fc19accc03eeb8ef4
Joshua Duenow <joshjduenow@gmail.com>
2024-01-08 13:42:34 -0600

    api is working and able to render 15 cards on the main page.

--- server/routes/card.router.js
+++ server/routes/card.router.js
@@ -3,42 +3,42 @@ const pool = require("../modules/pool");
 const axios = require("axios");
 const router = express.Router();
 require("dotenv").config();
-// const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY;
 
-router.get("/", (req, res) => {
+// router.get("/", (req, res) => {
 
-  const query = `
-      SELECT * FROM "all_cards"
-      ORDER BY "card_number" ASC;
-    `;
-  pool
-    .query(query)
-    .then((result) => {
-      res.send(result.rows);
-    })
-    .catch((err) => {
-      console.log("ERROR: Get all cards", err);
-      res.sendStatus(500);
-    });
-});
-
-module.exports = router;
-// router.get("/:id", (req, res) => {
-//   axios({
-//     method: "GET",
-//     url: `https://api.pokemontcg.io/v2/cards/select=name,number,images`,
-//     headers: {
-//       "X-Api-Key": API_KEY,
-//     },
-//   })
-//     .then((response) => {
-//       res.send(response.data);
+//   const query = `
+//       SELECT * FROM "all_cards"
+//       ORDER BY "card_number" ASC;
+//     `;
+//   pool
+//     .query(query)
+//     .then((result) => {
+//       res.send(result.rows);
 //     })
-//     .catch((error) => {
-//       console.log("Error in GET from apiRouter,", error);
+//     .catch((err) => {
+//       console.log("ERROR: Get all cards", err);
 //       res.sendStatus(500);
 //     });
 // });
+
+router.get("/", (req, res) => {
+  axios({
+    method: "GET",
+    url: `https://api.pokemontcg.io/v2/cards?select=name,number,images&&pageSize=15`,
+    headers: {
+      "X-Api-Key": API_KEY,
+    },
+  })
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log("Error in GET from card Router,", error);
+    res.sendStatus(500);
+  });
+});
+module.exports = router;
 
 // query to get card specifics?
 // &&q=select=name,number,images
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,25 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect } from "react";
+import "./CardList.css";
 import CardItem from "../CardItem/CardItem";
 
 export default function CardList() {
-    const dispatch = useDispatch();
-    const cards = useSelector((store) => store.cards)
-    
+  const dispatch = useDispatch();
+  const cards = useSelector((store) => store.cards);
 
-    useEffect(() => {
-        dispatch({ type: "GET_CARDS" });
-    }, []);
-    return(
-        <main>
-            <h4>Cards:</h4>
-            <div className="cardList">
-            {cards.map((card, i) => (
-                <div card={card.name[0]} key={i}>
-                    </div>
-            ))}
-            </div>
-        </main>
-        
-    )
+  useEffect(() => {
+    dispatch({ type: "GET_CARDS" });
+  }, []);
+  return (
+    <main>
+      <div className="cardList">
+        {cards.data && cards.data.map((card, i) => (
+          <ul key={i}>
+            <li>Name:{card.name}</li>
+            <li>Number:{card.number}</li>
+            <img src={card.images.small} />
+          </ul>
+        ))}
+      </div>
+    </main>
+  );
 }
